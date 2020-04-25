93322a2ec598ac9a0834394dd1d47ccf19471233
Alexandre Feio <alefeio@gmail.com>
2020-04-25 03:39:44 -0300

    alteração no cors

--- src/app.js
+++ src/app.js
@@ -25,11 +25,11 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(cors(
-      // {
-      //   origin: 'http://brazilianbp.com.br'
-      // }
-    ))
+    this.server.use(
+      cors({
+        origin: 'http://brazilianbp.com.br',
+      })
+    );
     this.server.use(express.json());
     this.server.use(
       '/files',
