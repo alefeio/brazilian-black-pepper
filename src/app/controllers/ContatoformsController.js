addb23a19dadcb4948b8ff5fa6345c4ce44367a0
Alexandre Feio <alefeio@gmail.com>
2020-04-27 00:06:29 -0300

    config redis

--- src/app/controllers/ContatoformsController.js
+++ src/app/controllers/ContatoformsController.js
@@ -6,20 +6,26 @@ import Queue from '../../lib/Queue';
 
 class ContatoformsController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      nome: Yup.string().required(),
-      email: Yup.string().email().required(),
-    });
+    try {
+      const schema = Yup.object().shape({
+        nome: Yup.string().required(),
+        email: Yup.string().email().required(),
+      });
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ erro: 'Falha na validação!' });
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ erro: 'Falha na validação!' });
+      }
+
+      const contato = await Contatoforms.create(req.body);
+
+      // await Queue.add(ContatoMail.key, contato);
+
+      return res.json(contato);
+    } catch (error) {
+      console.log(error);
+
+      return res.status(400).json({ erro: error });
     }
-
-    const contato = await Contatoforms.create(req.body);
-
-    await Queue.add(ContatoMail.key, contato);
-
-    return res.json(contato);
   }
 }
 
